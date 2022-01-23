26d23405d4f60550e603e87617314b9abcefc1bf
Raymond Jennings <shentino@gmail.com>
2022-01-22 22:14:40 -0800

    Updates, link with framer, create main event loop

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-game: main.o
-	g++ main.o -o game `sdl2-config --libs`
-main.o: main.cpp
+game: framer.o main.o
+	g++ framer.o main.o -o game `sdl2-config --libs`
+main.o: main.cpp framer.h
 	g++ -c main.cpp -o main.o `sdl2-config --cflags`
+framer.o: framer.cpp framer.h
+	g++ -c framer.cpp -o framer.o
--- main.cpp
+++ main.cpp
@@ -3,17 +3,50 @@
 
 #include <SDL2/SDL.h>
 
+#include "framer.h"
+
 using namespace std;
 
 int main(int argc, char *argv[], char *envp[])
 {
-	SDL_Window *win;
-
 	if (SDL_Init(SDL_INIT_VIDEO)) {
 		throw runtime_error(SDL_GetError());
 	}
 
-	win = SDL_CreateWindow("Tile game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE);
+	SDL_Window *win = SDL_CreateWindow("Tile game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE);
 
-	SDL_Delay(5000);
+	framer fr;
+
+	fr.set_framerate(60);
+
+	SDL_Surface *srf = SDL_GetWindowSurface(win);
+
+	SDL_Event evt;
+
+	for (;;) {
+		if (!SDL_WaitEvent(&evt)) {
+			throw runtime_error(SDL_GetError());
+		}
+
+		switch(evt.type) {
+		case SDL_WINDOWEVENT:
+			switch (evt.window.event) {
+			case SDL_WINDOWEVENT_SIZE_CHANGED:
+				srf = SDL_GetWindowSurface(win);
+				break;
+			}
+			break;
+
+		case SDL_QUIT:
+			goto out;
+		}
+
+		SDL_FillRect(srf, NULL, SDL_MapRGB(srf->format, 0xaa, 0x00, 0x55));
+		SDL_UpdateWindowSurface(win);
+
+		fr.next();
+	}
+
+out:
+	SDL_Quit();
 }
