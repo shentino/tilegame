a0f574fc7d9127013c4caf97fc78a8a07e3c0cfb
Raymond Jennings <shentino@gmail.com>
2022-04-15 02:33:05 -0700

    Begin implementing board drawer

--- board.cpp
+++ board.cpp
@@ -13,3 +13,15 @@ board::board(int new_width, int new_height)
 		tiles[i].terrain = 0;
 	}
 }
+
+void board::draw(gc *gc)
+{
+	/* for now just assume each tile needs to be 32 pixels wide */
+
+	/* eventually we'll need to consult the ui for zoom and scroll information */
+
+	int centerx, centery;
+
+	centerx = gc->get_width() / 2;
+	centery = gc->get_height() / 2;
+}
